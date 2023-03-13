d98cf9ff1a626700d2c0f6f86f6e1b6efec29559
Souvik Das <souvikvillain@icloud.com>
2023-03-13 07:30:20 +0530

    updated hello js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const user = {
+  name: "Souvik Das",
+  password: "hello123"
+}
+
+export default function handler(request, response) {
+  console.log(request)
+  let responseObj;
+  let requestName = request.body.name;
+  let requestPassword = request.body.password;
+  if (requestName == user.name) {
+    if (requestPassword == user.password) {
+      responseObj = { message : "User credentials valid"}
+    } else {
+      responseObj = { message : "Invalid password"}
+    }
+  } else {
+    responseObj = { message: "invalid username"}
+  }
+  response.status(200).json(responseObj)
 }
